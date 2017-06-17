c9a4eb0c022886635c1ae18f554137e7fa8079d1
jesse <jsoldat@hotmail.com>
2017-06-17 14:04:26 -0400

    in imperative style

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,4 +1,8 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Lesson } from './../shared/model/lesson';
+import { Course } from './../shared/model/course';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'course-detail',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent implements OnInit {
+  course: Course;
+  lessons: Lesson[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const courseUrl = params['id'];
+      this.db.list('courses', {
+        query: {
+          orderByChild: 'url',
+          equalTo: courseUrl
+        }
+      })
+      .map(data => data[0])
+      .subscribe(data => {
+        this.course = data;
+        this.db.list('lessons', {
+          query: {
+            orderByChild: 'courseId',
+            equalTo: data.$key
+          }
+        })
+        .subscribe(lessons => this.lessons = lessons);
+      });
+    });
   }
 
-}
+} 
